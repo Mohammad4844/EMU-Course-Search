e32376eab2df92579ad692af892780887bea5b8d
Mohammad <mohammadimaan484@gmail.com>
2023-08-21 15:58:47 -0400

    Added lambda function code

---
+++ database_preprocess/lambda_function.py
@@ -0,0 +1,30 @@
+import json
+import boto3
+import io
+from io import StringIO
+
+from extract_course_offerings import course_offerings
+from build_database import build_db
+
+s3_client = boto3.client('s3')
+
+def lambda_handler(event, context):
+    try:
+        s3_Bucket_Name = event["Records"][0]["s3"]["bucket"]["name"]
+        s3_File_Name = event["Records"][0]["s3"]["object"]["key"]
+        object = s3_client.get_object(Bucket=s3_Bucket_Name, Key=s3_File_Name)
+
+        body = object['Body']
+        body = body.read().decode('utf-8')
+        
+        offerings_data = course_offerings(body)
+        build_db(offerings_data)
+        
+    except Exception as err:
+        print(err)
+        
+    # TODO implement
+    return {
+        'statusCode': 200,
+        # 'body': json.dumps()
+    }
